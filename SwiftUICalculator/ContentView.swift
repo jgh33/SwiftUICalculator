972617abe4d57b1ab0c814870c72ca370590eb9d
Andy Jiao <1035390779@qq.com>
2019-11-28 17:48:16 +0800

    Initial Commit

---
+++ SwiftUICalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICalculator
+//
+//  Created by andy.g.h.jiao on 2019/11/28.
+//  Copyright © 2019 andy.g.h.jiao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
